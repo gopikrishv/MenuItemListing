e9329c33dcdfa40c273988e7a26df06fccf22277
gopikrishv <105410418+gopikrishv@users.noreply.github.com>
2022-05-18 23:46:00 +0530

    Update MenuItemController.cs

--- Controllers/MenuItemController.cs
+++ Controllers/MenuItemController.cs
@@ -16,9 +16,11 @@ namespace MenuItemListing.Controllers
         public List<MenuItem> GetItem()
         {
             List<MenuItem> MenuList = new List<MenuItem>()
-            {   new MenuItem() {Id=1, Name="Phone", Active=true, DateOfLaunch=new DateTime(2017,01,01), FreeDelivery=false,Price=500},
-                new MenuItem() {Id=2, Name="LapTop", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=200}
-
+            {   new MenuItem() {Id=1, Name="Biriyani", Active=true, DateOfLaunch=new DateTime(2017,01,01), FreeDelivery=false,Price=300},
+                new MenuItem() {Id=2, Name="Fried Rice", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=200},
+                new MenuItem() {Id=3, Name="Butter Chicken", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=150},
+                new MenuItem() {Id=4, Name="Chicken Noodles", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=200},
+                new MenuItem() {Id=5, Name="Fried Chicken", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=250}
             };
             return MenuList;
         }
@@ -28,7 +30,10 @@ namespace MenuItemListing.Controllers
         {
             List<MenuItem> MenuList = new List<MenuItem>()
             {   new MenuItem() {Id=1, Name="Biriyani", Active=true, DateOfLaunch=new DateTime(2017,01,01), FreeDelivery=false,Price=300},
-                new MenuItem() {Id=2, Name="Fried Rice", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=200}
+                new MenuItem() {Id=2, Name="Fried Rice", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=200},
+                new MenuItem() {Id=3, Name="Butter Chicken", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=150},
+                new MenuItem() {Id=4, Name="Chicken Noodles", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=200},
+                new MenuItem() {Id=5, Name="Fried Chicken", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=250}
 
             };
             MenuItem obj = MenuList.SingleOrDefault(item => item.Id == id);
